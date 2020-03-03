d16f5ef402d0524239056a133e2ee2ff75116e57
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-03-03 15:27:46 +0000

    refactor(config): vaccine_allocator config v5

---
+++ src/config/vaccine_allocator_v5_d3619_c58.py
@@ -0,0 +1,21 @@
+"""VaccineAllocator config v5d3619y2020."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class VaccineAllocatorConfig_v5d3619y2020:
+    enabled: bool = True
+    batch_size: int = 160
+    hidden_dim: int = 320
+    num_layers: int = 7
+    dropout: float = 0.5
+    learning_rate: float = 5.0e-04
+    max_epochs: int = 50
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "VaccineAllocatorConfig_v5d3619y2020":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
