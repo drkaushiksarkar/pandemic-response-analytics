6274333ee18ac9c3d54196625a697ab213899366
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-03-05 10:10:42 +0000

    feat(tests): vaccine_allocator tests v3

---
+++ src/tests/vaccine_allocator_v3_d8055_c182.py
@@ -0,0 +1,25 @@
+"""Tests for vaccine_allocator v3d8055y2020."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestVaccineAllocator_v3d8055y2020:
+    def test_init(self):
+        config = {"domain": "vaccine_allocator", "v": 3}
+        assert config["v"] == 3
+
+    def test_forward(self):
+        x = torch.randn(12, 24)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(9)]
+        assert len(batch) == 9
+
+    def test_metric(self):
+        pred = torch.randn(24)
+        target = torch.randn(24)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
