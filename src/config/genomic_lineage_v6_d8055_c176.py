06a4285a1ca8bebcbfe32adb4fcd70fc6f2a4d18
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-03-05 07:08:45 +0000

    fix(config): genomic_lineage config v6

---
+++ src/config/genomic_lineage_v6_d8055_c176.py
@@ -0,0 +1,21 @@
+"""GenomicLineage config v6d8055y2020."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class GenomicLineageConfig_v6d8055y2020:
+    enabled: bool = True
+    batch_size: int = 192
+    hidden_dim: int = 384
+    num_layers: int = 8
+    dropout: float = 0.6
+    learning_rate: float = 6.0e-04
+    max_epochs: int = 60
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "GenomicLineageConfig_v6d8055y2020":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
