4b0ffd74e45db168412809d35c1b4c6e1017571d
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-03-06 16:51:48 +0000

    chore(tests): quarantine_model tests v3

---
+++ src/tests/quarantine_model_v3_d424_c209.py
@@ -0,0 +1,25 @@
+"""Tests for quarantine_model v3d424y2020."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestQuarantineModel_v3d424y2020:
+    def test_init(self):
+        config = {"domain": "quarantine_model", "v": 3}
+        assert config["v"] == 3
+
+    def test_forward(self):
+        x = torch.randn(12, 24)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(9)]
+        assert len(batch) == 9
+
+    def test_metric(self):
+        pred = torch.randn(24)
+        target = torch.randn(24)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
