ff182164e87b9b24bab54ad49785332e45fcb67a
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-03-06 16:42:55 +0000

    docs(schemas): vaccine_allocator schemas v2

---
+++ src/schemas/vaccine_allocator_v2_d424_c145.py
@@ -0,0 +1,21 @@
+"""VaccineAllocator schemas v2d424y2020."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class VaccineAllocatorConfig_v2d424y2020:
+    enabled: bool = True
+    batch_size: int = 64
+    hidden_dim: int = 128
+    num_layers: int = 4
+    dropout: float = 0.2
+    learning_rate: float = 2.0e-04
+    max_epochs: int = 20
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "VaccineAllocatorConfig_v2d424y2020":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
