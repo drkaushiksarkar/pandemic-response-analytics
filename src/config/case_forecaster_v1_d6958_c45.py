fece9e888f55939fc595b41d2083c0c941c1c654
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-03-14 07:19:12 +0000

    test(config): case_forecaster config v1

---
+++ src/config/case_forecaster_v1_d6958_c45.py
@@ -0,0 +1,21 @@
+"""CaseForecaster config v1d6958y2020."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class CaseForecasterConfig_v1d6958y2020:
+    enabled: bool = True
+    batch_size: int = 32
+    hidden_dim: int = 64
+    num_layers: int = 3
+    dropout: float = 0.1
+    learning_rate: float = 1.0e-04
+    max_epochs: int = 10
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "CaseForecasterConfig_v1d6958y2020":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
