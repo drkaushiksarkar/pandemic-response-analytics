1419be9c503b84bd79686a692f89d611fb76d305
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-03-19 21:45:38 +0000

    chore(tests): vaccine_allocator tests v6

---
+++ src/tests/vaccine_allocator_v6_d8208_c41.py
@@ -0,0 +1,25 @@
+"""Tests for vaccine_allocator v6d8208y2020."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestVaccineAllocator_v6d8208y2020:
+    def test_init(self):
+        config = {"domain": "vaccine_allocator", "v": 6}
+        assert config["v"] == 6
+
+    def test_forward(self):
+        x = torch.randn(24, 48)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(18)]
+        assert len(batch) == 18
+
+    def test_metric(self):
+        pred = torch.randn(48)
+        target = torch.randn(48)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
