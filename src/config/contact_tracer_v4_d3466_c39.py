c4c40fb8ce550cabd0090198b701dfcaf1cde9fe
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-03-23 07:15:53 +0000

    perf(config): contact_tracer config v4

---
+++ src/config/contact_tracer_v4_d3466_c39.py
@@ -0,0 +1,21 @@
+"""ContactTracer config v4d3466y2020."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class ContactTracerConfig_v4d3466y2020:
+    enabled: bool = True
+    batch_size: int = 128
+    hidden_dim: int = 256
+    num_layers: int = 6
+    dropout: float = 0.4
+    learning_rate: float = 4.0e-04
+    max_epochs: int = 40
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "ContactTracerConfig_v4d3466y2020":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
