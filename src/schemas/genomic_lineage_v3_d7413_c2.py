3be741a94fed50c3df2b476ab4d53dd78cb8f354
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-03-26 15:38:52 +0000

    refactor(schemas): genomic_lineage schemas v3

---
+++ src/schemas/genomic_lineage_v3_d7413_c2.py
@@ -0,0 +1,21 @@
+"""GenomicLineage schemas v3d7413y2020."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class GenomicLineageConfig_v3d7413y2020:
+    enabled: bool = True
+    batch_size: int = 96
+    hidden_dim: int = 192
+    num_layers: int = 5
+    dropout: float = 0.3
+    learning_rate: float = 3.0e-04
+    max_epochs: int = 30
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "GenomicLineageConfig_v3d7413y2020":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
