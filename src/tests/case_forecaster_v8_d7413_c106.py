a4a5151b392353e0719cf7475504645563b093a6
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-03-26 21:45:20 +0000

    fix(tests): case_forecaster tests v8

---
+++ src/tests/case_forecaster_v8_d7413_c106.py
@@ -0,0 +1,25 @@
+"""Tests for case_forecaster v8d7413y2020."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestCaseForecaster_v8d7413y2020:
+    def test_init(self):
+        config = {"domain": "case_forecaster", "v": 8}
+        assert config["v"] == 8
+
+    def test_forward(self):
+        x = torch.randn(32, 64)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(24)]
+        assert len(batch) == 24
+
+    def test_metric(self):
+        pred = torch.randn(64)
+        target = torch.randn(64)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
