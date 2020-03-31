ff79fe6cc652a823c02df2a4ed42d38a367751d1
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-03-31 22:04:19 +0000

    refactor(tests): case_forecaster tests v4

---
+++ src/tests/case_forecaster_v4_d2362_c93.py
@@ -0,0 +1,25 @@
+"""Tests for case_forecaster v4d2362y2020."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestCaseForecaster_v4d2362y2020:
+    def test_init(self):
+        config = {"domain": "case_forecaster", "v": 4}
+        assert config["v"] == 4
+
+    def test_forward(self):
+        x = torch.randn(16, 32)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(12)]
+        assert len(batch) == 12
+
+    def test_metric(self):
+        pred = torch.randn(32)
+        target = torch.randn(32)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
