264232dd59526e8770c53666be46970b701732b2
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-04-01 22:23:04 +0000

    feat(config): quarantine_model config v7

---
+++ src/config/quarantine_model_v7_d6087_c42.py
@@ -0,0 +1,21 @@
+"""QuarantineModel config v7d6087y2020."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class QuarantineModelConfig_v7d6087y2020:
+    enabled: bool = True
+    batch_size: int = 224
+    hidden_dim: int = 448
+    num_layers: int = 9
+    dropout: float = 0.7
+    learning_rate: float = 7.0e-04
+    max_epochs: int = 70
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "QuarantineModelConfig_v7d6087y2020":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
