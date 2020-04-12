7e8b1d6ce24423ff9d56a51c1ebbc3628dcb757e
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-04-12 10:50:32 +0000

    refactor(schemas): contact_tracer schemas v2

---
+++ src/schemas/contact_tracer_v2_d1368_c37.py
@@ -0,0 +1,21 @@
+"""ContactTracer schemas v2d1368y2020."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class ContactTracerConfig_v2d1368y2020:
+    enabled: bool = True
+    batch_size: int = 64
+    hidden_dim: int = 128
+    num_layers: int = 4
+    dropout: float = 0.2
+    learning_rate: float = 2.0e-04
+    max_epochs: int = 20
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "ContactTracerConfig_v2d1368y2020":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
