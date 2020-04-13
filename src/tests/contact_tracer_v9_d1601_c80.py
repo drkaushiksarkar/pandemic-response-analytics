eae6299c34a06cb22c6435f152edb42cda1812ff
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-04-13 08:23:39 +0000

    test(tests): contact_tracer tests v9

---
+++ src/tests/contact_tracer_v9_d1601_c80.py
@@ -0,0 +1,25 @@
+"""Tests for contact_tracer v9d1601y2020."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestContactTracer_v9d1601y2020:
+    def test_init(self):
+        config = {"domain": "contact_tracer", "v": 9}
+        assert config["v"] == 9
+
+    def test_forward(self):
+        x = torch.randn(36, 72)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(27)]
+        assert len(batch) == 27
+
+    def test_metric(self):
+        pred = torch.randn(72)
+        target = torch.randn(72)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
