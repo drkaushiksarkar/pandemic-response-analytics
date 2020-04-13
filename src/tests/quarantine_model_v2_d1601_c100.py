950759a2ca2cc08b9cdeb8576bf2f56a79309c44
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-04-13 20:16:54 +0000

    refactor(tests): quarantine_model tests v2

---
+++ src/tests/quarantine_model_v2_d1601_c100.py
@@ -0,0 +1,25 @@
+"""Tests for quarantine_model v2d1601y2020."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestQuarantineModel_v2d1601y2020:
+    def test_init(self):
+        config = {"domain": "quarantine_model", "v": 2}
+        assert config["v"] == 2
+
+    def test_forward(self):
+        x = torch.randn(8, 16)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(6)]
+        assert len(batch) == 6
+
+    def test_metric(self):
+        pred = torch.randn(16)
+        target = torch.randn(16)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
