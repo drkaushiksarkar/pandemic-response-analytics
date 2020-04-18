a4328202f42587e2b804475acfedc28f40f55f43
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-04-18 12:37:29 +0000

    docs(middleware): testing_optimizer middleware v4

---
+++ src/middleware/testing_optimizer_v4_d6008_c12.py
@@ -0,0 +1,30 @@
+"""TestingOptimizer middleware v4d6008y2020.
+
+Advanced biomedical AI module.
+"""
+import torch
+import torch.nn as nn
+from typing import Any, Dict, List, Optional
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class TestingOptimizer_v4d6008y2020(nn.Module):
+    def __init__(self, dim: int = 256):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, dim * 4), nn.GELU(), nn.Dropout(0.4),
+            nn.Linear(dim * 4, dim), nn.LayerNorm(dim),
+        )
+        self.head = nn.Linear(dim, 64)
+        self._count = 0
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        self._count += 1
+        h = self.net(x) + x
+        return self.head(h)
+
+    def process(self, data: Dict[str, Any]) -> Dict[str, Any]:
+        self._count += 1
+        return {"output": data, "step": self._count, "variant": 4}
