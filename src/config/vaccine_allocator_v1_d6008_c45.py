7a095eab85b0fb215bd0ad7f834767142388ff18
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-04-18 08:21:37 +0000

    test(config): vaccine_allocator config v1

---
+++ src/config/vaccine_allocator_v1_d6008_c45.py
@@ -0,0 +1,21 @@
+"""VaccineAllocator config v1d6008y2020."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class VaccineAllocatorConfig_v1d6008y2020:
+    enabled: bool = True
+    batch_size: int = 32
+    hidden_dim: int = 64
+    num_layers: int = 3
+    dropout: float = 0.1
+    learning_rate: float = 1.0e-04
+    max_epochs: int = 10
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "VaccineAllocatorConfig_v1d6008y2020":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
