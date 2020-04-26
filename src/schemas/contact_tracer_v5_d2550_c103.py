67ee06e85322a07892d4a6d3ef2f1f98765ab3e3
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-04-26 20:48:23 +0000

    docs(schemas): contact_tracer schemas v5

---
+++ src/schemas/contact_tracer_v5_d2550_c103.py
@@ -0,0 +1,21 @@
+"""ContactTracer schemas v5d2550y2020."""
+from dataclasses import dataclass
+from typing import Any, Dict, Optional
+
+
+@dataclass
+class ContactTracerConfig_v5d2550y2020:
+    enabled: bool = True
+    batch_size: int = 160
+    hidden_dim: int = 320
+    num_layers: int = 7
+    dropout: float = 0.5
+    learning_rate: float = 5.0e-04
+    max_epochs: int = 50
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, Any]) -> "ContactTracerConfig_v5d2550y2020":
+        return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
+
+    def validate(self) -> bool:
+        return self.batch_size > 0 and self.hidden_dim > 0
