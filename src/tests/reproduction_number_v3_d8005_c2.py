210741a77721247ba9c9054ca4a4104bfe2a65c8
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-05-01 13:05:42 +0000

    refactor(tests): reproduction_number tests v3

---
+++ src/tests/reproduction_number_v3_d8005_c2.py
@@ -0,0 +1,25 @@
+"""Tests for reproduction_number v3d8005y2020."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestReproductionNumber_v3d8005y2020:
+    def test_init(self):
+        config = {"domain": "reproduction_number", "v": 3}
+        assert config["v"] == 3
+
+    def test_forward(self):
+        x = torch.randn(12, 24)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(9)]
+        assert len(batch) == 9
+
+    def test_metric(self):
+        pred = torch.randn(24)
+        target = torch.randn(24)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
