73596e1be65a93571b00925a422a3e01b0c12fdf
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-05-09 09:56:46 +0000

    feat(tests): genomic_lineage tests v8

---
+++ src/tests/genomic_lineage_v8_d1000_c7.py
@@ -0,0 +1,25 @@
+"""Tests for genomic_lineage v8d1000y2020."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestGenomicLineage_v8d1000y2020:
+    def test_init(self):
+        config = {"domain": "genomic_lineage", "v": 8}
+        assert config["v"] == 8
+
+    def test_forward(self):
+        x = torch.randn(32, 64)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(24)]
+        assert len(batch) == 24
+
+    def test_metric(self):
+        pred = torch.randn(64)
+        target = torch.randn(64)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
