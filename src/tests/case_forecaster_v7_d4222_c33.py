e49ed2a6a276fe0c861c45779e69dc8658562028
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2020-05-12 11:52:45 +0000

    docs(tests): case_forecaster tests v7

---
+++ src/tests/case_forecaster_v7_d4222_c33.py
@@ -0,0 +1,25 @@
+"""Tests for case_forecaster v7d4222y2020."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestCaseForecaster_v7d4222y2020:
+    def test_init(self):
+        config = {"domain": "case_forecaster", "v": 7}
+        assert config["v"] == 7
+
+    def test_forward(self):
+        x = torch.randn(28, 56)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(21)]
+        assert len(batch) == 21
+
+    def test_metric(self):
+        pred = torch.randn(56)
+        target = torch.randn(56)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
