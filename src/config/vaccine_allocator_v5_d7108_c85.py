9816d3b31c65f292b2b9904345a98aa731f7925e
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-02-04 09:54:02 +0000

    fix(config): vaccine_allocator config v5

--- src/config/vaccine_allocator_v5_d7108_c85.py
+++ src/config/vaccine_allocator_v5_d7108_c85.py
@@ -1,10 +1,10 @@
-"""VaccineAllocator config v5d7108y2020."""
+"""VaccineAllocator config v5d7108y2021."""
 from dataclasses import dataclass
 from typing import Any, Dict, Optional
 
 
 @dataclass
-class VaccineAllocatorConfig_v5d7108y2020:
+class VaccineAllocatorConfig_v5d7108y2021:
     enabled: bool = True
     batch_size: int = 160
     hidden_dim: int = 320
@@ -14,7 +14,7 @@ class VaccineAllocatorConfig_v5d7108y2020:
     max_epochs: int = 50
 
     @classmethod
-    def from_dict(cls, d: Dict[str, Any]) -> "VaccineAllocatorConfig_v5d7108y2020":
+    def from_dict(cls, d: Dict[str, Any]) -> "VaccineAllocatorConfig_v5d7108y2021":
         return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
 
     def validate(self) -> bool:
