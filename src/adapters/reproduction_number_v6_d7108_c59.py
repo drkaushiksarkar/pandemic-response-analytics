de678a57cc76184a92027e627e95bc19c46d1aac
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-02-04 08:00:58 +0000

    test(adapters): reproduction_number adapters v6

--- src/adapters/reproduction_number_v6_d7108_c59.py
+++ src/adapters/reproduction_number_v6_d7108_c59.py
@@ -1,4 +1,4 @@
-"""ReproductionNumber adapters v6d7108y2020.
+"""ReproductionNumber adapters v6d7108y2021.
 
 Advanced biomedical AI module.
 """
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class ReproductionNumber_v6d7108y2020(nn.Module):
+class ReproductionNumber_v6d7108y2021(nn.Module):
     def __init__(self, dim: int = 384):
         super().__init__()
         self.net = nn.Sequential(
