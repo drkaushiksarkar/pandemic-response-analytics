1691a4b82f4dfa27a7d21d5a513c5864afdb57ef
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-02-04 22:37:26 +0000

    fix(adapters): quarantine_model adapters v3

--- src/adapters/quarantine_model_v3_d7108_c29.py
+++ src/adapters/quarantine_model_v3_d7108_c29.py
@@ -1,4 +1,4 @@
-"""QuarantineModel adapters v3d7108y2020.
+"""QuarantineModel adapters v3d7108y2021.
 
 Advanced biomedical AI module.
 """
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class QuarantineModel_v3d7108y2020(nn.Module):
+class QuarantineModel_v3d7108y2021(nn.Module):
     def __init__(self, dim: int = 192):
         super().__init__()
         self.net = nn.Sequential(
