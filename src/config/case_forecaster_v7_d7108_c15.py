f3cc22ca6431dd94de5638256f3416bb19f16801
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-02-04 12:54:35 +0000

    fix(config): case_forecaster config v7

--- src/config/case_forecaster_v7_d7108_c15.py
+++ src/config/case_forecaster_v7_d7108_c15.py
@@ -1,10 +1,10 @@
-"""CaseForecaster config v7d7108y2020."""
+"""CaseForecaster config v7d7108y2021."""
 from dataclasses import dataclass
 from typing import Any, Dict, Optional
 
 
 @dataclass
-class CaseForecasterConfig_v7d7108y2020:
+class CaseForecasterConfig_v7d7108y2021:
     enabled: bool = True
     batch_size: int = 224
     hidden_dim: int = 448
@@ -14,7 +14,7 @@ class CaseForecasterConfig_v7d7108y2020:
     max_epochs: int = 70
 
     @classmethod
-    def from_dict(cls, d: Dict[str, Any]) -> "CaseForecasterConfig_v7d7108y2020":
+    def from_dict(cls, d: Dict[str, Any]) -> "CaseForecasterConfig_v7d7108y2021":
         return cls(**{k: v for k, v in d.items() if k in cls.__dataclass_fields__})
 
     def validate(self) -> bool:
