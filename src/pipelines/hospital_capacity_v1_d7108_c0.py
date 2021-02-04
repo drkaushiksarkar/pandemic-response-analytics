ad9ae8c1abe759aecd7b0fa3d19520f708199f68
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-02-04 10:40:09 +0000

    feat(pipelines): hospital_capacity pipelines v1

--- src/pipelines/hospital_capacity_v1_d7108_c0.py
+++ src/pipelines/hospital_capacity_v1_d7108_c0.py
@@ -1,4 +1,4 @@
-"""HospitalCapacity pipelines v1d7108y2020.
+"""HospitalCapacity pipelines v1d7108y2021.
 
 Advanced biomedical AI module.
 """
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class HospitalCapacity_v1d7108y2020(nn.Module):
+class HospitalCapacity_v1d7108y2021(nn.Module):
     def __init__(self, dim: int = 64):
         super().__init__()
         self.net = nn.Sequential(
