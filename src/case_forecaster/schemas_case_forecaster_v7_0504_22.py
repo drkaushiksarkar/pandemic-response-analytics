905e13ecab9f24f3ac08cc5d50870f5f3bd46543
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-04 14:23:23 +0530

    perf(case_forecaster): case_forecaster schemas v7

---
+++ src/case_forecaster/schemas_case_forecaster_v7_0504_22.py
@@ -0,0 +1,28 @@
+"""Configuration for case_forecaster v7d59y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
+from pathlib import Path
+
+
+@dataclass
+class CaseForecasterConfig_v7d59y2021:
+    """Configuration for case_forecaster variant 7."""
+    name: str = "case_forecaster"
+    version: str = "7.59.0"
+    num_layers: int = 14
+    hidden_dim: int = 448
+    learning_rate: float = 0.000700
+    batch_size: int = 112
+    max_epochs: int = 350
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/case_forecaster/v7d59y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+    metadata: Dict[str, str] = field(default_factory=lambda: {"created": "2021", "domain": "case_forecaster"})
+    early_stopping_patience: int = 35
+    weight_decay: float = 1e-9
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        assert 0 < self.learning_rate < 1
+        return True
