69d9faa6566b1fb003a6d3c197115aea7f85396d
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-10 08:42:45 +0530

    test(contact_tracer): contact_tracer config v4

---
+++ src/contact_tracer/config_contact_tracer_v4_0510_3.py
@@ -0,0 +1,28 @@
+"""Configuration for contact_tracer v4d62y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
+from pathlib import Path
+
+
+@dataclass
+class ContactTracerConfig_v4d62y2021:
+    """Configuration for contact_tracer variant 4."""
+    name: str = "contact_tracer"
+    version: str = "4.62.0"
+    num_layers: int = 8
+    hidden_dim: int = 256
+    learning_rate: float = 0.000400
+    batch_size: int = 64
+    max_epochs: int = 200
+    dropout: float = 0.4
+    checkpoint_dir: Path = Path("checkpoints/contact_tracer/v4d62y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+    metadata: Dict[str, str] = field(default_factory=lambda: {"created": "2021", "domain": "contact_tracer"})
+    early_stopping_patience: int = 20
+    weight_decay: float = 1e-6
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        assert 0 < self.learning_rate < 1
+        return True
