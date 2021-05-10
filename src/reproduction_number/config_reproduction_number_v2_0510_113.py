ef31340510aa4240d20ea2d25f48ce3aa6f219c5
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-10 22:01:35 +0530

    docs(reproduction_number): reproduction_number config v2

---
+++ src/reproduction_number/config_reproduction_number_v2_0510_113.py
@@ -0,0 +1,28 @@
+"""Configuration for reproduction_number v2d62y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
+from pathlib import Path
+
+
+@dataclass
+class ReproductionNumberConfig_v2d62y2021:
+    """Configuration for reproduction_number variant 2."""
+    name: str = "reproduction_number"
+    version: str = "2.62.0"
+    num_layers: int = 4
+    hidden_dim: int = 128
+    learning_rate: float = 0.000200
+    batch_size: int = 32
+    max_epochs: int = 100
+    dropout: float = 0.2
+    checkpoint_dir: Path = Path("checkpoints/reproduction_number/v2d62y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+    metadata: Dict[str, str] = field(default_factory=lambda: {"created": "2021", "domain": "reproduction_number"})
+    early_stopping_patience: int = 10
+    weight_decay: float = 1e-4
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        assert 0 < self.learning_rate < 1
+        return True
