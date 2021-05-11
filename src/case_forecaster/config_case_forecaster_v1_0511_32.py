ce21d93cef7fd0aa669049882caa2962d4cd1ebf
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-11 10:19:18 +0530

    refactor(case_forecaster): case_forecaster config v1

---
+++ src/case_forecaster/config_case_forecaster_v1_0511_32.py
@@ -0,0 +1,28 @@
+"""Configuration for case_forecaster v1d63y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
+from pathlib import Path
+
+
+@dataclass
+class CaseForecasterConfig_v1d63y2021:
+    """Configuration for case_forecaster variant 1."""
+    name: str = "case_forecaster"
+    version: str = "1.63.0"
+    num_layers: int = 2
+    hidden_dim: int = 64
+    learning_rate: float = 0.000100
+    batch_size: int = 16
+    max_epochs: int = 50
+    dropout: float = 0.1
+    checkpoint_dir: Path = Path("checkpoints/case_forecaster/v1d63y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+    metadata: Dict[str, str] = field(default_factory=lambda: {"created": "2021", "domain": "case_forecaster"})
+    early_stopping_patience: int = 5
+    weight_decay: float = 1e-3
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        assert 0 < self.learning_rate < 1
+        return True
