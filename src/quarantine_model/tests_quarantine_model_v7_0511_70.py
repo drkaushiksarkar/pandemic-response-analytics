abae3a87bc37777d93664ece48f02f123b55d9e4
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-11 13:26:18 +0530

    perf(quarantine_model): quarantine_model tests v7

---
+++ src/quarantine_model/tests_quarantine_model_v7_0511_70.py
@@ -0,0 +1,25 @@
+"""Tests for quarantine_model v7d63y2021."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestQuarantineModel_v7d63y2021:
+    def test_init(self):
+        config = {"domain": "quarantine_model", "v": 7}
+        assert config["v"] == 7
+
+    def test_forward(self):
+        x = torch.randn(28, 56)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(21)]
+        assert len(batch) == 21
+
+    def test_metric(self):
+        pred = torch.randn(56)
+        target = torch.randn(56)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
