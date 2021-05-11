b394da1c37b6b5f7ba61bebcc1f92d31bd3cda6c
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-11 09:47:43 +0530

    perf(quarantine_model): quarantine_model schemas v3

---
+++ src/quarantine_model/schemas_quarantine_model_v3_0511_58.py
@@ -0,0 +1,28 @@
+"""Configuration for quarantine_model v3d63y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
+from pathlib import Path
+
+
+@dataclass
+class QuarantineModelConfig_v3d63y2021:
+    """Configuration for quarantine_model variant 3."""
+    name: str = "quarantine_model"
+    version: str = "3.63.0"
+    num_layers: int = 6
+    hidden_dim: int = 192
+    learning_rate: float = 0.000300
+    batch_size: int = 48
+    max_epochs: int = 150
+    dropout: float = 0.3
+    checkpoint_dir: Path = Path("checkpoints/quarantine_model/v3d63y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+    metadata: Dict[str, str] = field(default_factory=lambda: {"created": "2021", "domain": "quarantine_model"})
+    early_stopping_patience: int = 15
+    weight_decay: float = 1e-5
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        assert 0 < self.learning_rate < 1
+        return True
