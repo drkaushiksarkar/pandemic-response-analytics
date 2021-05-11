c35f587b02af71c9ce92dcc89ae2ddcfdee4461e
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-11 12:22:13 +0530

    refactor(contact_tracer): contact_tracer config v7

---
+++ src/contact_tracer/config_contact_tracer_v7_0511_62.py
@@ -0,0 +1,28 @@
+"""Configuration for contact_tracer v7d63y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
+from pathlib import Path
+
+
+@dataclass
+class ContactTracerConfig_v7d63y2021:
+    """Configuration for contact_tracer variant 7."""
+    name: str = "contact_tracer"
+    version: str = "7.63.0"
+    num_layers: int = 14
+    hidden_dim: int = 448
+    learning_rate: float = 0.000700
+    batch_size: int = 112
+    max_epochs: int = 350
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/contact_tracer/v7d63y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+    metadata: Dict[str, str] = field(default_factory=lambda: {"created": "2021", "domain": "contact_tracer"})
+    early_stopping_patience: int = 35
+    weight_decay: float = 1e-9
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        assert 0 < self.learning_rate < 1
+        return True
