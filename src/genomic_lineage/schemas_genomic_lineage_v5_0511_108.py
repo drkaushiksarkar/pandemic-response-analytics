8739d2580bf598c596992fdff2e9c6d486d2694d
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-11 07:34:52 +0530

    feat(genomic_lineage): genomic_lineage schemas v5

---
+++ src/genomic_lineage/schemas_genomic_lineage_v5_0511_108.py
@@ -0,0 +1,28 @@
+"""Configuration for genomic_lineage v5d63y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
+from pathlib import Path
+
+
+@dataclass
+class GenomicLineageConfig_v5d63y2021:
+    """Configuration for genomic_lineage variant 5."""
+    name: str = "genomic_lineage"
+    version: str = "5.63.0"
+    num_layers: int = 10
+    hidden_dim: int = 320
+    learning_rate: float = 0.000500
+    batch_size: int = 80
+    max_epochs: int = 250
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/genomic_lineage/v5d63y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+    metadata: Dict[str, str] = field(default_factory=lambda: {"created": "2021", "domain": "genomic_lineage"})
+    early_stopping_patience: int = 25
+    weight_decay: float = 1e-7
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        assert 0 < self.learning_rate < 1
+        return True
