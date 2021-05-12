0671bda116da365903ac2480b9129b80e7926491
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-12 17:53:24 +0530

    test(reproduction_number): reproduction_number schemas v8

---
+++ src/reproduction_number/schemas_reproduction_number_v8_0512_87.py
@@ -0,0 +1,28 @@
+"""Configuration for reproduction_number v8d64y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
+from pathlib import Path
+
+
+@dataclass
+class ReproductionNumberConfig_v8d64y2021:
+    """Configuration for reproduction_number variant 8."""
+    name: str = "reproduction_number"
+    version: str = "8.64.0"
+    num_layers: int = 16
+    hidden_dim: int = 512
+    learning_rate: float = 0.000800
+    batch_size: int = 128
+    max_epochs: int = 400
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/reproduction_number/v8d64y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+    metadata: Dict[str, str] = field(default_factory=lambda: {"created": "2021", "domain": "reproduction_number"})
+    early_stopping_patience: int = 40
+    weight_decay: float = 1e-10
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        assert 0 < self.learning_rate < 1
+        return True
