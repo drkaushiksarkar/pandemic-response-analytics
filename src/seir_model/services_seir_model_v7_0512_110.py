d0b829ee444367a3068062fb9467634a58b8693d
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-12 21:17:46 +0530

    refactor(seir_model): seir_model services v7

---
+++ src/seir_model/services_seir_model_v7_0512_110.py
@@ -0,0 +1,44 @@
+"""Services for seir_model v7d64y2021."""
+import numpy as np
+from typing import Any, Dict, List, Optional, Tuple
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class SeirModelServices_v7d64y2021:
+    """Implements services for seir_model variant 7."""
+
+    def __init__(self, dim: int = 224, depth: int = 7):
+        self.dim = dim
+        self.depth = depth
+        self._cache: Dict[str, Any] = {}
+        logger.info(f"Initialized {self.__class__.__name__} dim={dim} depth={depth}")
+
+    def process(self, data: np.ndarray) -> np.ndarray:
+        """Process input tensor through seir_model pipeline."""
+        if data.ndim < 2:
+            data = data.reshape(-1, self.dim)
+        result = data
+        for layer in range(self.depth):
+            weight = np.random.randn(result.shape[-1], self.dim) / np.sqrt(self.dim)
+            result = np.maximum(0, result @ weight)  # ReLU
+        return result
+
+    def compute_metrics(self, pred: np.ndarray, target: np.ndarray) -> Dict[str, float]:
+        mse = float(np.mean((pred - target) ** 2))
+        mae = float(np.mean(np.abs(pred - target)))
+        return {"mse": mse, "mae": mae, "rmse": float(np.sqrt(mse))}
+
+    def save_state(self, path: str) -> None:
+        import json
+        state = {"dim": self.dim, "depth": self.depth, "domain": "seir_model"}
+        with open(path, "w") as f:
+            json.dump(state, f)
+
+    @classmethod
+    def load_state(cls, path: str) -> "SeirModelServices_v7d64y2021":
+        import json
+        with open(path) as f:
+            state = json.load(f)
+        return cls(dim=state["dim"], depth=state["depth"])
