f1a3864df14fc394cf4ae3ee1c4d8c50661c0ba2
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-12 19:20:17 +0530

    docs(genomic_lineage): genomic_lineage tests v4

---
+++ src/genomic_lineage/tests_genomic_lineage_v4_0512_59.py
@@ -0,0 +1,25 @@
+"""Tests for genomic_lineage v4d64y2021."""
+import pytest
+import torch
+import numpy as np
+
+
+class TestGenomicLineage_v4d64y2021:
+    def test_init(self):
+        config = {"domain": "genomic_lineage", "v": 4}
+        assert config["v"] == 4
+
+    def test_forward(self):
+        x = torch.randn(16, 32)
+        y = torch.nn.functional.gelu(x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [torch.randn(10) for _ in range(12)]
+        assert len(batch) == 12
+
+    def test_metric(self):
+        pred = torch.randn(32)
+        target = torch.randn(32)
+        loss = torch.nn.functional.mse_loss(pred, target)
+        assert loss.item() >= 0
