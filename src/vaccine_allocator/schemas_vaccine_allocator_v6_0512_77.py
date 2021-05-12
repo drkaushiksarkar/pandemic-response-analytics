269f8416757774dffe88b159b1d83835bf4a513e
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-12 16:32:06 +0530

    docs(vaccine_allocator): vaccine_allocator schemas v6

---
+++ src/vaccine_allocator/schemas_vaccine_allocator_v6_0512_77.py
@@ -0,0 +1,28 @@
+"""Configuration for vaccine_allocator v6d64y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
+from pathlib import Path
+
+
+@dataclass
+class VaccineAllocatorConfig_v6d64y2021:
+    """Configuration for vaccine_allocator variant 6."""
+    name: str = "vaccine_allocator"
+    version: str = "6.64.0"
+    num_layers: int = 12
+    hidden_dim: int = 384
+    learning_rate: float = 0.000600
+    batch_size: int = 96
+    max_epochs: int = 300
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/vaccine_allocator/v6d64y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+    metadata: Dict[str, str] = field(default_factory=lambda: {"created": "2021", "domain": "vaccine_allocator"})
+    early_stopping_patience: int = 30
+    weight_decay: float = 1e-8
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        assert 0 < self.learning_rate < 1
+        return True
