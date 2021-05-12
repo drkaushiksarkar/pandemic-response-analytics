76895233001a4fbc849c3f7600119a9314bb0f5a
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-12 21:30:17 +0530

    test(reproduction_number): reproduction_number adapters v8

---
+++ src/reproduction_number/adapters_reproduction_number_v8_0512_63.py
@@ -0,0 +1,44 @@
+"""Adapters for reproduction_number v8d64y2021."""
+import numpy as np
+from typing import Any, Dict, List, Optional, Tuple
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class ReproductionNumberAdapters_v8d64y2021:
+    """Implements adapters for reproduction_number variant 8."""
+
+    def __init__(self, dim: int = 256, depth: int = 8):
+        self.dim = dim
+        self.depth = depth
+        self._cache: Dict[str, Any] = {}
+        logger.info(f"Initialized {self.__class__.__name__} dim={dim} depth={depth}")
+
+    def process(self, data: np.ndarray) -> np.ndarray:
+        """Process input tensor through reproduction_number pipeline."""
+        if data.ndim < 2:
+            data = data.reshape(-1, self.dim)
+        result = data
+        for layer in range(self.depth):
+            weight = np.random.randn(result.shape[-1], self.dim) / np.sqrt(self.dim)
+            result = np.maximum(0, result @ weight)  # ReLU
+        return result
+
+    def compute_metrics(self, pred: np.ndarray, target: np.ndarray) -> Dict[str, float]:
+        mse = float(np.mean((pred - target) ** 2))
+        mae = float(np.mean(np.abs(pred - target)))
+        return {"mse": mse, "mae": mae, "rmse": float(np.sqrt(mse))}
+
+    def save_state(self, path: str) -> None:
+        import json
+        state = {"dim": self.dim, "depth": self.depth, "domain": "reproduction_number"}
+        with open(path, "w") as f:
+            json.dump(state, f)
+
+    @classmethod
+    def load_state(cls, path: str) -> "ReproductionNumberAdapters_v8d64y2021":
+        import json
+        with open(path) as f:
+            state = json.load(f)
+        return cls(dim=state["dim"], depth=state["depth"])
