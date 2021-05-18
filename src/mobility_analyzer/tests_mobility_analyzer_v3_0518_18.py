98550bd445ee627b49bb40d074186179cf8b949d
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-18 18:48:06 +0530

    feat(mobility_analyzer): mobility_analyzer tests v3

---
+++ src/mobility_analyzer/tests_mobility_analyzer_v3_0518_18.py
@@ -0,0 +1,24 @@
+"""Tests for mobility_analyzer v3d68y2021."""
+import pytest
+import numpy as np
+
+
+class TestMobilityAnalyzer_v3d68y2021:
+    def test_init(self):
+        config = {"domain": "mobility_analyzer", "v": 3}
+        assert config["v"] == 3
+
+    def test_forward(self):
+        x = np.random.randn(12, 24)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(9)]
+        assert len(batch) == 9
+
+    def test_metric(self):
+        pred = np.random.randn(24)
+        target = np.random.randn(24)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
