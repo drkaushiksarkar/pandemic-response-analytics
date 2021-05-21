39a076a65bb9f245adfcfa07f1bfdc40253d14a0
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-21 16:33:15 +0530

    feat(testing_optimizer): testing_optimizer tests v5

---
+++ src/testing_optimizer/tests_testing_optimizer_v5_0521_36.py
@@ -0,0 +1,24 @@
+"""Tests for testing_optimizer v5d70y2021."""
+import pytest
+import numpy as np
+
+
+class TestTestingOptimizer_v5d70y2021:
+    def test_init(self):
+        config = {"domain": "testing_optimizer", "v": 5}
+        assert config["v"] == 5
+
+    def test_forward(self):
+        x = np.random.randn(20, 40)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(15)]
+        assert len(batch) == 15
+
+    def test_metric(self):
+        pred = np.random.randn(40)
+        target = np.random.randn(40)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
