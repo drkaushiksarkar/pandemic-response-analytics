5ef8e6b22032dcfaa5ecd4bf248228f5ba8247c7
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-23 22:04:40 +0530

    docs(genomic_lineage): genomic_lineage config v2

---
+++ src/genomic_lineage/config_genomic_lineage_v2_0523_17.py
@@ -0,0 +1,23 @@
+"""Configuration for genomic_lineage v2d71y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class GenomicLineageConfig_v2d71y2021:
+    name: str = "genomic_lineage"
+    version: str = "2.71.0"
+    num_layers: int = 4
+    hidden_dim: int = 128
+    learning_rate: float = 0.000200
+    batch_size: int = 32
+    max_epochs: int = 100
+    dropout: float = 0.2
+    checkpoint_dir: Path = Path("checkpoints/genomic_lineage/v2d71y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
