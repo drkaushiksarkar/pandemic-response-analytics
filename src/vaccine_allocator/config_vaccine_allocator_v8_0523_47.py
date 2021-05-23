f399d3c0d4621b4649f78714e3086dfcc7fb7f76
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-23 08:26:51 +0530

    docs(vaccine_allocator): vaccine_allocator config v8

---
+++ src/vaccine_allocator/config_vaccine_allocator_v8_0523_47.py
@@ -0,0 +1,23 @@
+"""Configuration for vaccine_allocator v8d71y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class VaccineAllocatorConfig_v8d71y2021:
+    name: str = "vaccine_allocator"
+    version: str = "8.71.0"
+    num_layers: int = 16
+    hidden_dim: int = 512
+    learning_rate: float = 0.000800
+    batch_size: int = 128
+    max_epochs: int = 400
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/vaccine_allocator/v8d71y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
