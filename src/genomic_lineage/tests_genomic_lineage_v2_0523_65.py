1b71fa101e6949de5c0738db5ee771a0f0c29de6
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-23 14:28:24 +0530

    docs(genomic_lineage): genomic_lineage tests v2

---
+++ src/genomic_lineage/tests_genomic_lineage_v2_0523_65.py
@@ -0,0 +1,24 @@
+"""Tests for genomic_lineage v2d71y2021."""
+import pytest
+import numpy as np
+
+
+class TestGenomicLineage_v2d71y2021:
+    def test_init(self):
+        config = {"domain": "genomic_lineage", "v": 2}
+        assert config["v"] == 2
+
+    def test_forward(self):
+        x = np.random.randn(8, 16)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(6)]
+        assert len(batch) == 6
+
+    def test_metric(self):
+        pred = np.random.randn(16)
+        target = np.random.randn(16)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
