9a5f60aa9e37b326b97a0736babe2ae65ff762d2
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-24 16:39:32 +0530

    refactor(reproduction_number): reproduction_number schemas v3

---
+++ src/reproduction_number/schemas_reproduction_number_v3_0524_2.py
@@ -0,0 +1,23 @@
+"""Configuration for reproduction_number v3d72y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class ReproductionNumberConfig_v3d72y2021:
+    name: str = "reproduction_number"
+    version: str = "3.72.0"
+    num_layers: int = 6
+    hidden_dim: int = 192
+    learning_rate: float = 0.000300
+    batch_size: int = 48
+    max_epochs: int = 150
+    dropout: float = 0.3
+    checkpoint_dir: Path = Path("checkpoints/reproduction_number/v3d72y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
