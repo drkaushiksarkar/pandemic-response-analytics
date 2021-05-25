389d15277d464e02655a5e2ce1149ab7c2de5340
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-25 08:50:57 +0530

    feat(hospital_capacity): hospital_capacity pipelines v5

---
+++ src/hospital_capacity/pipelines_hospital_capacity_v5_0525_60.py
@@ -0,0 +1,27 @@
+"""Pipeline for hospital_capacity v5d73y2021."""
+import logging
+from typing import Any, Dict, List
+
+logger = logging.getLogger(__name__)
+
+
+class HospitalCapacityPipeline_v5d73y2021:
+    def __init__(self, config: Dict[str, Any]):
+        self.config = config
+        self.stages: List[str] = []
+
+    def add_stage(self, name: str, fn: callable) -> "HospitalCapacityPipeline_v5d73y2021":
+        self.stages.append(name)
+        return self
+
+    def validate_input(self, data: Any) -> bool:
+        if data is None:
+            raise ValueError("Input data cannot be None")
+        return True
+
+    def run(self, data: Any) -> Dict[str, Any]:
+        self.validate_input(data)
+        results = {"input_size": len(data) if hasattr(data, "__len__") else 1}
+        for stage in self.stages:
+            results[stage] = "completed"
+        return results
