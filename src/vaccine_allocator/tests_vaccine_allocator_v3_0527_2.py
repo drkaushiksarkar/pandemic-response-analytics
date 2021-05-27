25a17d73819d04f432cf57673b1763b6a4313e63
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-27 13:52:26 +0530

    refactor(vaccine_allocator): vaccine_allocator tests v3

---
+++ src/vaccine_allocator/tests_vaccine_allocator_v3_0527_2.py
@@ -0,0 +1,24 @@
+"""Tests for vaccine_allocator v3d74y2021."""
+import pytest
+import numpy as np
+
+
+class TestVaccineAllocator_v3d74y2021:
+    def test_init(self):
+        config = {"domain": "vaccine_allocator", "v": 3}
+        assert config["v"] == 3
+
+    def test_forward(self):
+        x = np.random.randn(12, 24)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(9)]
+        assert len(batch) == 9
+
+    def test_metric(self):
+        pred = np.random.randn(24)
+        target = np.random.randn(24)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
