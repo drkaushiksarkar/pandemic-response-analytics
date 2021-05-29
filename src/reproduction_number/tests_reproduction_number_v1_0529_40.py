c23aaf95f17e6406a1965e28c25b167cf327ec23
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-05-29 12:14:17 +0530

    perf(reproduction_number): reproduction_number tests v1

---
+++ src/reproduction_number/tests_reproduction_number_v1_0529_40.py
@@ -0,0 +1,24 @@
+"""Tests for reproduction_number v1d76y2021."""
+import pytest
+import numpy as np
+
+
+class TestReproductionNumber_v1d76y2021:
+    def test_init(self):
+        config = {"domain": "reproduction_number", "v": 1}
+        assert config["v"] == 1
+
+    def test_forward(self):
+        x = np.random.randn(4, 8)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(3)]
+        assert len(batch) == 3
+
+    def test_metric(self):
+        pred = np.random.randn(8)
+        target = np.random.randn(8)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
