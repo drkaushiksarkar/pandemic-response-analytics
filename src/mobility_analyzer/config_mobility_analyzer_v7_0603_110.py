5695835374e259430dec37a70b45f6554f5b3643
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-03 20:51:40 +0530

    refactor(mobility_analyzer): mobility_analyzer config v7

---
+++ src/mobility_analyzer/config_mobility_analyzer_v7_0603_110.py
@@ -0,0 +1,23 @@
+"""Configuration for mobility_analyzer v7d78y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class MobilityAnalyzerConfig_v7d78y2021:
+    name: str = "mobility_analyzer"
+    version: str = "7.78.0"
+    num_layers: int = 14
+    hidden_dim: int = 448
+    learning_rate: float = 0.000700
+    batch_size: int = 112
+    max_epochs: int = 350
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/mobility_analyzer/v7d78y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
