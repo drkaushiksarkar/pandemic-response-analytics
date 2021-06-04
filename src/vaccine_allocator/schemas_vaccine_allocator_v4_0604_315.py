ce49b83aacacb78ffc17ca297bb3c7fb9c9352b4
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-04 07:59:37 +0530

    test(vaccine_allocator): vaccine_allocator schemas v4

---
+++ src/vaccine_allocator/schemas_vaccine_allocator_v4_0604_315.py
@@ -0,0 +1,23 @@
+"""Configuration for vaccine_allocator v4d79y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class VaccineAllocatorConfig_v4d79y2021:
+    name: str = "vaccine_allocator"
+    version: str = "4.79.0"
+    num_layers: int = 8
+    hidden_dim: int = 256
+    learning_rate: float = 0.000400
+    batch_size: int = 64
+    max_epochs: int = 200
+    dropout: float = 0.4
+    checkpoint_dir: Path = Path("checkpoints/vaccine_allocator/v4d79y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
