e1e0cce8f13542f1c72a235dd9f7d9f3fed7bdd1
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-05 07:41:35 +0530

    refactor(testing_optimizer): testing_optimizer schemas v7

---
+++ src/testing_optimizer/schemas_testing_optimizer_v7_0605_14.py
@@ -0,0 +1,23 @@
+"""Configuration for testing_optimizer v7d80y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class TestingOptimizerConfig_v7d80y2021:
+    name: str = "testing_optimizer"
+    version: str = "7.80.0"
+    num_layers: int = 14
+    hidden_dim: int = 448
+    learning_rate: float = 0.000700
+    batch_size: int = 112
+    max_epochs: int = 350
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/testing_optimizer/v7d80y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
