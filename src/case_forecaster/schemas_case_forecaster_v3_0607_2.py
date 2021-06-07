8806dcbd7ef4bb9c4b5b25c2516331a15f5e6e96
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-07 20:02:13 +0530

    refactor(case_forecaster): case_forecaster schemas v3

---
+++ src/case_forecaster/schemas_case_forecaster_v3_0607_2.py
@@ -0,0 +1,23 @@
+"""Configuration for case_forecaster v3d82y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class CaseForecasterConfig_v3d82y2021:
+    name: str = "case_forecaster"
+    version: str = "3.82.0"
+    num_layers: int = 6
+    hidden_dim: int = 192
+    learning_rate: float = 0.000300
+    batch_size: int = 48
+    max_epochs: int = 150
+    dropout: float = 0.3
+    checkpoint_dir: Path = Path("checkpoints/case_forecaster/v3d82y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
