f970b25b71724b2d72a17e4b3c1efd7d130ea4d7
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-08 17:55:39 +0530

    docs(vaccine_allocator): vaccine_allocator tests v8

---
+++ src/vaccine_allocator/tests_vaccine_allocator_v8_0608_23.py
@@ -0,0 +1,24 @@
+"""Tests for vaccine_allocator v8d83y2021."""
+import pytest
+import numpy as np
+
+
+class TestVaccineAllocator_v8d83y2021:
+    def test_init(self):
+        config = {"domain": "vaccine_allocator", "v": 8}
+        assert config["v"] == 8
+
+    def test_forward(self):
+        x = np.random.randn(32, 64)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(24)]
+        assert len(batch) == 24
+
+    def test_metric(self):
+        pred = np.random.randn(64)
+        target = np.random.randn(64)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
