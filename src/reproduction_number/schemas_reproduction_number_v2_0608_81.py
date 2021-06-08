57316e453d9c6a7a8672a9d17fc9507816ebb849
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-08 20:35:49 +0530

    test(reproduction_number): reproduction_number schemas v2

---
+++ src/reproduction_number/schemas_reproduction_number_v2_0608_81.py
@@ -0,0 +1,23 @@
+"""Configuration for reproduction_number v2d83y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class ReproductionNumberConfig_v2d83y2021:
+    name: str = "reproduction_number"
+    version: str = "2.83.0"
+    num_layers: int = 4
+    hidden_dim: int = 128
+    learning_rate: float = 0.000200
+    batch_size: int = 32
+    max_epochs: int = 100
+    dropout: float = 0.2
+    checkpoint_dir: Path = Path("checkpoints/reproduction_number/v2d83y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
