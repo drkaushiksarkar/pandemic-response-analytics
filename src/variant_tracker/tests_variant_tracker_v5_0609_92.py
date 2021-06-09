cdab66d7d9cbb76844cc5bfdf2475316cb163e5b
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-09 23:27:25 +0530

    refactor(variant_tracker): variant_tracker tests v5

---
+++ src/variant_tracker/tests_variant_tracker_v5_0609_92.py
@@ -0,0 +1,24 @@
+"""Tests for variant_tracker v5d84y2021."""
+import pytest
+import numpy as np
+
+
+class TestVariantTracker_v5d84y2021:
+    def test_init(self):
+        config = {"domain": "variant_tracker", "v": 5}
+        assert config["v"] == 5
+
+    def test_forward(self):
+        x = np.random.randn(20, 40)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(15)]
+        assert len(batch) == 15
+
+    def test_metric(self):
+        pred = np.random.randn(40)
+        target = np.random.randn(40)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
