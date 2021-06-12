763a79630f746afbbaf97634baf4a8d678eddec3
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-12 07:20:07 +0530

    refactor(hospital_capacity): hospital_capacity services v5

---
+++ src/hospital_capacity/services_hospital_capacity_v5_0612_92.py
@@ -0,0 +1,27 @@
+"""Services for hospital_capacity v5d85y2021."""
+import numpy as np
+from typing import Any, Dict
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class HospitalCapacityServices_v5d85y2021:
+    def __init__(self, dim: int = 160, depth: int = 5):
+        self.dim = dim
+        self.depth = depth
+        self._cache: Dict[str, Any] = {}
+
+    def process(self, data: np.ndarray) -> np.ndarray:
+        if data.ndim < 2:
+            data = data.reshape(-1, self.dim)
+        result = data
+        for layer in range(self.depth):
+            w = np.random.randn(result.shape[-1], self.dim) / np.sqrt(self.dim)
+            result = np.maximum(0, result @ w)
+        return result
+
+    def compute_metrics(self, pred: np.ndarray, target: np.ndarray) -> Dict[str, float]:
+        mse = float(np.mean((pred - target) ** 2))
+        mae = float(np.mean(np.abs(pred - target)))
+        return {"mse": mse, "mae": mae, "rmse": float(np.sqrt(mse))}
