e9332d5b199fa246b549aa6afa7b0189287a62c8
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-12 22:01:02 +0530

    test(genomic_lineage): genomic_lineage schemas v8

---
+++ src/genomic_lineage/schemas_genomic_lineage_v8_0612_39.py
@@ -0,0 +1,23 @@
+"""Configuration for genomic_lineage v8d85y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class GenomicLineageConfig_v8d85y2021:
+    name: str = "genomic_lineage"
+    version: str = "8.85.0"
+    num_layers: int = 16
+    hidden_dim: int = 512
+    learning_rate: float = 0.000800
+    batch_size: int = 128
+    max_epochs: int = 400
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/genomic_lineage/v8d85y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
