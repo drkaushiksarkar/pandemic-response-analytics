f523b03cfa7383b8af7df3cc88bff4f5d28b6a3a
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-12 11:35:28 +0530

    docs(contact_tracer): contact_tracer config v2

---
+++ src/contact_tracer/config_contact_tracer_v2_0612_113.py
@@ -0,0 +1,23 @@
+"""Configuration for contact_tracer v2d85y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class ContactTracerConfig_v2d85y2021:
+    name: str = "contact_tracer"
+    version: str = "2.85.0"
+    num_layers: int = 4
+    hidden_dim: int = 128
+    learning_rate: float = 0.000200
+    batch_size: int = 32
+    max_epochs: int = 100
+    dropout: float = 0.2
+    checkpoint_dir: Path = Path("checkpoints/contact_tracer/v2d85y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
