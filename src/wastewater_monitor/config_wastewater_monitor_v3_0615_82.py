a3b3deae7c1a7fee7f62c0ff0328491ec400e4d0
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-15 07:18:02 +0530

    perf(wastewater_monitor): wastewater_monitor config v3

---
+++ src/wastewater_monitor/config_wastewater_monitor_v3_0615_82.py
@@ -0,0 +1,23 @@
+"""Configuration for wastewater_monitor v3d86y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class WastewaterMonitorConfig_v3d86y2021:
+    name: str = "wastewater_monitor"
+    version: str = "3.86.0"
+    num_layers: int = 6
+    hidden_dim: int = 192
+    learning_rate: float = 0.000300
+    batch_size: int = 48
+    max_epochs: int = 150
+    dropout: float = 0.3
+    checkpoint_dir: Path = Path("checkpoints/wastewater_monitor/v3d86y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
