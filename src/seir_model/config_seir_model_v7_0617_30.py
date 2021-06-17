f13a57d0cbde17628554bb2bd5b9d7054c1897e6
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-17 17:07:22 +0530

    feat(seir_model): seir_model config v7

---
+++ src/seir_model/config_seir_model_v7_0617_30.py
@@ -0,0 +1,23 @@
+"""Configuration for seir_model v7d88y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class SeirModelConfig_v7d88y2021:
+    name: str = "seir_model"
+    version: str = "7.88.0"
+    num_layers: int = 14
+    hidden_dim: int = 448
+    learning_rate: float = 0.000700
+    batch_size: int = 112
+    max_epochs: int = 350
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/seir_model/v7d88y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
