3b1527bf657ca561aafa52b83e2cab55ed537760
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-21 13:48:09 +0530

    refactor(testing_optimizer): testing_optimizer config v3

---
+++ src/testing_optimizer/config_testing_optimizer_v3_0621_98.py
@@ -0,0 +1,23 @@
+"""Configuration for testing_optimizer v3d90y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class TestingOptimizerConfig_v3d90y2021:
+    name: str = "testing_optimizer"
+    version: str = "3.90.0"
+    num_layers: int = 6
+    hidden_dim: int = 192
+    learning_rate: float = 0.000300
+    batch_size: int = 48
+    max_epochs: int = 150
+    dropout: float = 0.3
+    checkpoint_dir: Path = Path("checkpoints/testing_optimizer/v3d90y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
