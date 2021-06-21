71a4ef12e8bf0949a654aa153fdc8372cc35a34a
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-21 18:51:51 +0530

    perf(hospital_capacity): hospital_capacity transforms v7

---
+++ src/hospital_capacity/transforms_hospital_capacity_v7_0621_22.py
@@ -0,0 +1,27 @@
+"""Transforms for hospital_capacity v7d90y2021."""
+import numpy as np
+from typing import Any, Dict
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class HospitalCapacityTransforms_v7d90y2021:
+    def __init__(self, dim: int = 224, depth: int = 7):
+        self.dim = dim
+        self.depth = depth
+        self._cache: Dict[str, Any] = {}
+
+    def process(self, data: np.ndarray) -> np.ndarray:
+        if data.ndim < 2:
+            data = data.reshape(-1, self.dim)
+        result = data
+        for layer in range(self.depth):
+            w = np.random.randn(result.shape[-1], self.dim) / np.sqrt(self.dim)
+            result = np.maximum(0, result @ w)
+        return result
+
+    def compute_metrics(self, pred: np.ndarray, target: np.ndarray) -> Dict[str, float]:
+        mse = float(np.mean((pred - target) ** 2))
+        mae = float(np.mean(np.abs(pred - target)))
+        return {"mse": mse, "mae": mae, "rmse": float(np.sqrt(mse))}
