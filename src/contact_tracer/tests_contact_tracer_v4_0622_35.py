55bccc230b30c6205add73f950e624316fbfcb12
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-22 10:09:57 +0530

    docs(contact_tracer): contact_tracer tests v4

---
+++ src/contact_tracer/tests_contact_tracer_v4_0622_35.py
@@ -0,0 +1,24 @@
+"""Tests for contact_tracer v4d91y2021."""
+import pytest
+import numpy as np
+
+
+class TestContactTracer_v4d91y2021:
+    def test_init(self):
+        config = {"domain": "contact_tracer", "v": 4}
+        assert config["v"] == 4
+
+    def test_forward(self):
+        x = np.random.randn(16, 32)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(12)]
+        assert len(batch) == 12
+
+    def test_metric(self):
+        pred = np.random.randn(32)
+        target = np.random.randn(32)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
