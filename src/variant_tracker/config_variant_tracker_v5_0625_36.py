c0c0d7c23cd02f4e51a9e6b91b6f00365d741b6d
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-25 18:19:18 +0530

    feat(variant_tracker): variant_tracker config v5

---
+++ src/variant_tracker/config_variant_tracker_v5_0625_36.py
@@ -0,0 +1,23 @@
+"""Configuration for variant_tracker v5d92y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class VariantTrackerConfig_v5d92y2021:
+    name: str = "variant_tracker"
+    version: str = "5.92.0"
+    num_layers: int = 10
+    hidden_dim: int = 320
+    learning_rate: float = 0.000500
+    batch_size: int = 80
+    max_epochs: int = 250
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/variant_tracker/v5d92y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
