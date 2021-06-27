7e6d59cb90b9140e7a95089f4482e9a949f35eed
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-27 17:06:35 +0530

    test(quarantine_model): quarantine_model tests v2

---
+++ src/quarantine_model/tests_quarantine_model_v2_0627_33.py
@@ -0,0 +1,24 @@
+"""Tests for quarantine_model v2d93y2021."""
+import pytest
+import numpy as np
+
+
+class TestQuarantineModel_v2d93y2021:
+    def test_init(self):
+        config = {"domain": "quarantine_model", "v": 2}
+        assert config["v"] == 2
+
+    def test_forward(self):
+        x = np.random.randn(8, 16)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(6)]
+        assert len(batch) == 6
+
+    def test_metric(self):
+        pred = np.random.randn(16)
+        target = np.random.randn(16)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
