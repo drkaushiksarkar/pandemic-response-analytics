d9aede004de5bace64ba713a99acabda239614bc
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-27 16:57:56 +0530

    fix(case_forecaster): case_forecaster tests v8

---
+++ src/case_forecaster/tests_case_forecaster_v8_0627_103.py
@@ -0,0 +1,24 @@
+"""Tests for case_forecaster v8d93y2021."""
+import pytest
+import numpy as np
+
+
+class TestCaseForecaster_v8d93y2021:
+    def test_init(self):
+        config = {"domain": "case_forecaster", "v": 8}
+        assert config["v"] == 8
+
+    def test_forward(self):
+        x = np.random.randn(32, 64)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(24)]
+        assert len(batch) == 24
+
+    def test_metric(self):
+        pred = np.random.randn(64)
+        target = np.random.randn(64)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
