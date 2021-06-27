2931477757ddb154ab66460c19876f3d60dded5d
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-27 12:35:04 +0530

    fix(case_forecaster): case_forecaster config v8

---
+++ src/case_forecaster/config_case_forecaster_v8_0627_55.py
@@ -0,0 +1,23 @@
+"""Configuration for case_forecaster v8d93y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class CaseForecasterConfig_v8d93y2021:
+    name: str = "case_forecaster"
+    version: str = "8.93.0"
+    num_layers: int = 16
+    hidden_dim: int = 512
+    learning_rate: float = 0.000800
+    batch_size: int = 128
+    max_epochs: int = 400
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/case_forecaster/v8d93y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
