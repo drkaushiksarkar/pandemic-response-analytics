be624573393d87c9c8cc041fa89b7eebe852249c
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-27 22:06:10 +0530

    fix(contact_tracer): contact_tracer tests v2

---
+++ src/contact_tracer/tests_contact_tracer_v2_0627_73.py
@@ -0,0 +1,24 @@
+"""Tests for contact_tracer v2d93y2021."""
+import pytest
+import numpy as np
+
+
+class TestContactTracer_v2d93y2021:
+    def test_init(self):
+        config = {"domain": "contact_tracer", "v": 2}
+        assert config["v"] == 2
+
+    def test_forward(self):
+        x = np.random.randn(8, 16)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(6)]
+        assert len(batch) == 6
+
+    def test_metric(self):
+        pred = np.random.randn(16)
+        target = np.random.randn(16)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
