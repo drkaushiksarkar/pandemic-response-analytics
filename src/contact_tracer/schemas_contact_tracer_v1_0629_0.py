d0468b671e11a18f3f0a19f01b7330fec31a098c
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-06-29 11:12:22 +0530

    feat(contact_tracer): contact_tracer schemas v1

---
+++ src/contact_tracer/schemas_contact_tracer_v1_0629_0.py
@@ -0,0 +1,23 @@
+"""Configuration for contact_tracer v1d94y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class ContactTracerConfig_v1d94y2021:
+    name: str = "contact_tracer"
+    version: str = "1.94.0"
+    num_layers: int = 2
+    hidden_dim: int = 64
+    learning_rate: float = 0.000100
+    batch_size: int = 16
+    max_epochs: int = 50
+    dropout: float = 0.1
+    checkpoint_dir: Path = Path("checkpoints/contact_tracer/v1d94y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
