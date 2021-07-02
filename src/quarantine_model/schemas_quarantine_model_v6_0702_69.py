17bdcc16bcdf74921a6f536700f11247fff50c81
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-07-02 10:46:31 +0530

    test(quarantine_model): quarantine_model schemas v6

---
+++ src/quarantine_model/schemas_quarantine_model_v6_0702_69.py
@@ -0,0 +1,23 @@
+"""Configuration for quarantine_model v6d95y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class QuarantineModelConfig_v6d95y2021:
+    name: str = "quarantine_model"
+    version: str = "6.95.0"
+    num_layers: int = 12
+    hidden_dim: int = 384
+    learning_rate: float = 0.000600
+    batch_size: int = 96
+    max_epochs: int = 300
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/quarantine_model/v6d95y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
