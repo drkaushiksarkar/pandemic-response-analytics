f22375c6f7146808c8dd484169aa4ee8a1872fd8
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-07-03 23:43:19 +0530

    feat(mobility_analyzer): mobility_analyzer config v3

---
+++ src/mobility_analyzer/config_mobility_analyzer_v3_0703_42.py
@@ -0,0 +1,23 @@
+"""Configuration for mobility_analyzer v3d96y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class MobilityAnalyzerConfig_v3d96y2021:
+    name: str = "mobility_analyzer"
+    version: str = "3.96.0"
+    num_layers: int = 6
+    hidden_dim: int = 192
+    learning_rate: float = 0.000300
+    batch_size: int = 48
+    max_epochs: int = 150
+    dropout: float = 0.3
+    checkpoint_dir: Path = Path("checkpoints/mobility_analyzer/v3d96y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
