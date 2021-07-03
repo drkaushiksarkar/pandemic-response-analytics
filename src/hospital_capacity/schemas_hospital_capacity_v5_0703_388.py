898d8ffddda247336c5fc52f12a28eead85df599
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-07-03 07:23:17 +0530

    perf(hospital_capacity): hospital_capacity schemas v5

---
+++ src/hospital_capacity/schemas_hospital_capacity_v5_0703_388.py
@@ -0,0 +1,23 @@
+"""Configuration for hospital_capacity v5d96y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class HospitalCapacityConfig_v5d96y2021:
+    name: str = "hospital_capacity"
+    version: str = "5.96.0"
+    num_layers: int = 10
+    hidden_dim: int = 320
+    learning_rate: float = 0.000500
+    batch_size: int = 80
+    max_epochs: int = 250
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/hospital_capacity/v5d96y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
