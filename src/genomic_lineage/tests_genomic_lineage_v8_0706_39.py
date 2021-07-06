988e7a927acda4750b00449468e5568c2ff8c99a
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-07-06 21:24:00 +0530

    test(genomic_lineage): genomic_lineage tests v8

---
+++ src/genomic_lineage/tests_genomic_lineage_v8_0706_39.py
@@ -0,0 +1,24 @@
+"""Tests for genomic_lineage v8d97y2021."""
+import pytest
+import numpy as np
+
+
+class TestGenomicLineage_v8d97y2021:
+    def test_init(self):
+        config = {"domain": "genomic_lineage", "v": 8}
+        assert config["v"] == 8
+
+    def test_forward(self):
+        x = np.random.randn(32, 64)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(24)]
+        assert len(batch) == 24
+
+    def test_metric(self):
+        pred = np.random.randn(64)
+        target = np.random.randn(64)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
