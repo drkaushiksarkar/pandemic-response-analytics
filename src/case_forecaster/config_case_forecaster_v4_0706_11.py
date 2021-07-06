79c7dfb3d2a0ce27db1354c56792d3c1cdc4547c
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-07-06 20:07:04 +0530

    docs(case_forecaster): case_forecaster config v4

---
+++ src/case_forecaster/config_case_forecaster_v4_0706_11.py
@@ -0,0 +1,23 @@
+"""Configuration for case_forecaster v4d97y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class CaseForecasterConfig_v4d97y2021:
+    name: str = "case_forecaster"
+    version: str = "4.97.0"
+    num_layers: int = 8
+    hidden_dim: int = 256
+    learning_rate: float = 0.000400
+    batch_size: int = 64
+    max_epochs: int = 200
+    dropout: float = 0.4
+    checkpoint_dir: Path = Path("checkpoints/case_forecaster/v4d97y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
