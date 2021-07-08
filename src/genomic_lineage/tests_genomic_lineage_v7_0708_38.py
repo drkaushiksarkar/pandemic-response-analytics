6cef61ce6a68e93278a29c1d169bc434281eedc4
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-07-08 11:00:58 +0530

    refactor(genomic_lineage): genomic_lineage tests v7

---
+++ src/genomic_lineage/tests_genomic_lineage_v7_0708_38.py
@@ -0,0 +1,24 @@
+"""Tests for genomic_lineage v7d98y2021."""
+import pytest
+import numpy as np
+
+
+class TestGenomicLineage_v7d98y2021:
+    def test_init(self):
+        config = {"domain": "genomic_lineage", "v": 7}
+        assert config["v"] == 7
+
+    def test_forward(self):
+        x = np.random.randn(28, 56)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(21)]
+        assert len(batch) == 21
+
+    def test_metric(self):
+        pred = np.random.randn(56)
+        target = np.random.randn(56)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
