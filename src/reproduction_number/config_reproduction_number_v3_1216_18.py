0c34938e6beeaa0a396d1a5f2ca0796cb50b8c46
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2021-12-16 22:46:03 +0530

    feat(reproduction_number): reproduction_number config v3

---
+++ src/reproduction_number/config_reproduction_number_v3_1216_18.py
@@ -0,0 +1,23 @@
+"""Configuration for reproduction_number v3d190y2021."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class ReproductionNumberConfig_v3d190y2021:
+    name: str = "reproduction_number"
+    version: str = "3.190.0"
+    num_layers: int = 6
+    hidden_dim: int = 192
+    learning_rate: float = 0.000300
+    batch_size: int = 48
+    max_epochs: int = 150
+    dropout: float = 0.3
+    checkpoint_dir: Path = Path("checkpoints/reproduction_number/v3d190y2021")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
